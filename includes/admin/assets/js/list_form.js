45a5717b6454b583a7d0f56350ca5bb9d6846044
hassantafreshi <hasan.tafreshi@gmail.com>
2021-02-11 12:59:24 +0330

    4th update for publish

--- includes/admin/assets/js/list_form.js
+++ includes/admin/assets/js/list_form.js
@@ -697,10 +697,10 @@ function fun_show_help__emsFormBuilder(){
       2:{title:'How to config Easy form Builder.',url:'https://www.youtube.com/embed/DEQNHMPT0rQ'},
       3:{title:'How to get google re-captcha and add to Easy Form Builder.',url:'https://www.youtube.com/embed/a1jbMqunzkQ'},
       4:{title:'How to activate the alert email of a new response.',url:'https://www.youtube.com/embed/So2RAzu-OHU'},
-      5:{title:'How to Create a Form with Easy form Builder.',url:'https://www.youtube.com/embed/7jS01CEtbDg'},
+      5:{title:'How to create a Form with Easy form Builder.',url:'https://www.youtube.com/embed/7jS01CEtbDg'},
       6:{title:'How to activation Tracking Code in Easy form Builder.',url:'https://www.youtube.com/embed/im3aKby4E14'},
       7:{title:'How to work with panel of Easy form Builder.',url:'https://www.youtube.com/embed/7jS01CEtbDg'},
-      8:{title:'How to Add tracking Form to a post or page.',url:'https://www.youtube.com/embed/c1_gCFihrH8'},
+      8:{title:'How to add tracking Form to a post or page.',url:'https://www.youtube.com/embed/c1_gCFihrH8'},
       9:{title:'How to find a response by tracking code.',url:'https://www.youtube.com/embed/vqKi9BJbO7k'}
    /*    10:{title:'How to test Pro version on localhost',url:'https://www.youtube.com/embed/RZTyFcjZTSM'}, */
     }
