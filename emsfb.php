b365f5719cb1f3d1f2c574032e0421e646b37062
Kolja Nolte <kolja.nolte@gmail.com>
2021-03-06 15:15:49 +0100

    Test commit
    Signed-off-by: Kolja Nolte <kolja.nolte@gmail.com>

--- emsfb.php
+++ emsfb.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Plugin Name: Easy Form Builder
- * Plugin URI: https://whitestudio.team/
- * Description: This plugin Create from (multi step form) by drag and drop form wizard and produce form with tracking code ability , Free[Limited] version
- * Version: 1.31.6
- * Author: WhiteStudio
- * Author URI: https://whitestudio.team/
- * Text Domain: easy-form-builder
- * Domain Path: /languages
+ * Plugin Name:         Easy Form Builder
+ * Plugin URI:          https://whitestudio.team/
+ * Description:         This plugin Create from (multi step form) by drag and drop form wizard and produce form with tracking code ability , Free[Limited] version
+ * Version:             1.31.6
+ * Author:              WhiteStudio
+ * Author URI:          https://whitestudio.team/
+ * Text Domain:         easy-form-builder
+ * Domain Path:         /languages
  */
 
 // Define EMSFB_PLUGIN_FILE.
