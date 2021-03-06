78081ca0b640ed36f056548f30660380669d3276
Kolja Nolte <kolja.nolte@gmail.com>
2021-03-06 16:54:19 +0100

    Test commit; Direct access prevention; Cosmetics
    Signed-off-by: Kolja Nolte <kolja.nolte@gmail.com>

--- emsfb.php
+++ emsfb.php
@@ -1,26 +1,27 @@
 <?php
 /**
  * Plugin Name:         Easy Form Builder
- * Plugin URI:          https://whitestudio.team/
+ * Plugin URI:          https://whitestudio.team
  * Description:         This plugin Create from (multi step form) by drag and drop form wizard and produce form with tracking code ability , Free[Limited] version
  * Version:             1.31.6
  * Author:              WhiteStudio
- * Author URI:          https://whitestudio.team/
+ * Author URI:          https://whitestudio.team
  * Text Domain:         easy-form-builder
  * Domain Path:         /languages
  */
 
-// Define EMSFB_PLUGIN_FILE.
-if ( ! defined( 'EMSFB_PLUGIN_FILE' ) ) {
-	define( 'EMSFB_PLUGIN_FILE', __FILE__ );
+/** Prevent this file from being accessed directly */
+if (!defined('ABSPATH')) {
+    die("Direct access of plugin files is not allowed.");
 }
 
-/**
- * Load main class.
- */
+/** Define EMSFB_PLUGIN_FILE */
+if (!defined('EMSFB_PLUGIN_FILE')) {
+    define('EMSFB_PLUGIN_FILE', __FILE__);
+}
+
+/** Load main class */
 require 'includes/class-Emsfb.php';
 
-/**
- * Main instance of plugin.
- */
-new Emsfb();
+/** Main instance of plugin */
+$emsfb = new Emsfb();
