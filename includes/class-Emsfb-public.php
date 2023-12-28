c6636590d10e0f8d8c5bc82b386c463084ce771f
Hassan Tafreshi <hasan.tafreshi@gmail.com>
2023-12-28 03:48:07 +0330

    fix

--- includes/class-Emsfb-public.php
+++ includes/class-Emsfb-public.php
@@ -5813,7 +5813,7 @@ class _Public {
 		';
 	}
 
-	public function send_json_success_efb($response, $arg=null,){
+	public function send_json_success_efb($response, $arg=null){
 		
 		header('Content-Type: application/json; charset=' . get_option('blog_charset'));
 		print(json_encode(array('success' => true, 'data' => $response)));
