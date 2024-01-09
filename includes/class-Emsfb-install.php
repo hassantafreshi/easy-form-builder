4951ac3d541d8e3af692863a5e4dccbbd3f042c8
Presskopp <cherrmann@gmx.de>
2024-01-09 13:24:05 +0100

    Update class-Emsfb-install.php
    set $upgrade_path variable
    
    use require_once only once
    
    combine all SQL statements into one

--- includes/class-Emsfb-install.php
+++ includes/class-Emsfb-install.php
@@ -26,89 +26,80 @@ class Install {
 		$table_name_status = $wpdb->prefix . "emsfb_stts_";
 
 		$charset_collate = $wpdb->get_charset_collate();
+		$upgrade_path = ABSPATH . 'wp-admin/includes/upgrade.php';
 
 		
 			
-						$sql = "CREATE TABLE IF NOT EXISTS {$table_name_stng} (
-							`id` int(1) NOT NULL AUTO_INCREMENT,
-							`setting` text COLLATE utf8mb4_unicode_ci NOT NULL, 
-							`date` datetime  DEFAULT CURRENT_TIMESTAMP NOT NULL,
-							`edit_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-							PRIMARY KEY  (id)
-			
-						) {$charset_collate};";
-						require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-						dbDelta( $sql );
-			
-						$sql = "CREATE TABLE IF NOT EXISTS {$table_name} (
-							`form_id` int(11) NOT NULL AUTO_INCREMENT,
-							`form_name` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`form_structer` MEDIUMTEXT COLLATE utf8mb4_unicode_ci NOT NULL,
-							`form_email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL, 
-							`form_type` varchar(15) COLLATE utf8mb4_unicode_ci NULL DEFAULT  'form',
-							`form_created_by` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`form_access_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL, 
-							`form_create_date` datetime  DEFAULT CURRENT_TIMESTAMP NOT NULL,		
-							PRIMARY KEY  (form_id)
-						) {$charset_collate};";
-						require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-						dbDelta( $sql );
-			
-						$sql = "CREATE TABLE IF NOT EXISTS {$table_name_msg} (
-							`msg_id` int(11) NOT NULL AUTO_INCREMENT,
-							`form_id` int(11) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`track` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`ip` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`form_title_x` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`content` MEDIUMTEXT COLLATE utf8mb4_unicode_ci NOT NULL,		
-							`date` datetime  DEFAULT CURRENT_TIMESTAMP NOT NULL,		
-							`read_date` datetime  DEFAULT CURRENT_TIMESTAMP,		
-							`read_` int(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`read_by` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-							PRIMARY KEY  (msg_id)
-						) {$charset_collate};";
-			
-						require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-						dbDelta( $sql );
+						$sql = "
+							    CREATE TABLE IF NOT EXISTS {$table_name_stng} (
+							        `id` int(1) NOT NULL AUTO_INCREMENT,
+							        `setting` text COLLATE utf8mb4_unicode_ci NOT NULL, 
+							        `date` datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+							        `edit_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+							        `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+							        PRIMARY KEY  (id)
+							    ) {$charset_collate};
+							
+							    CREATE TABLE IF NOT EXISTS {$table_name} (
+							        `form_id` int(11) NOT NULL AUTO_INCREMENT,
+							        `form_name` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+							        `form_structer` MEDIUMTEXT COLLATE utf8mb4_unicode_ci NOT NULL,
+							        `form_email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL, 
+							        `form_type` varchar(15) COLLATE utf8mb4_unicode_ci NULL DEFAULT 'form',
+							        `form_created_by` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
+							        `form_access_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL, 
+							        `form_create_date` datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,        
+							        PRIMARY KEY  (form_id)
+							    ) {$charset_collate};
 
-						$sql = "CREATE TABLE IF NOT EXISTS {$table_name_rsp} (
-							`rsp_id` int(20) NOT NULL AUTO_INCREMENT,
-							`msg_id` int(11) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`ip` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`content` text COLLATE utf8mb4_unicode_ci NOT NULL,		
-							`date` datetime  DEFAULT CURRENT_TIMESTAMP NOT NULL,		
-							`read_by` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`read_date` datetime  DEFAULT CURRENT_TIMESTAMP,		
-							`read_` int(10) COLLATE utf8mb4_unicode_ci NOT NULL, 
-							`reader_ip` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`rsp_by` int(1) COLLATE utf8mb4_unicode_ci NOT NULL, 
-							PRIMARY KEY  (rsp_id)
-						) {$charset_collate};";
-			
-					require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-					dbDelta( $sql );
+							    CREATE TABLE IF NOT EXISTS {$table_name_msg} (
+								`msg_id` int(11) NOT NULL AUTO_INCREMENT,
+								`form_id` int(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`track` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`ip` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`form_title_x` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`content` MEDIUMTEXT COLLATE utf8mb4_unicode_ci NOT NULL,		
+								`date` datetime  DEFAULT CURRENT_TIMESTAMP NOT NULL,		
+								`read_date` datetime  DEFAULT CURRENT_TIMESTAMP,		
+								`read_` int(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`read_by` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+								PRIMARY KEY  (msg_id)
+							    ) {$charset_collate};
+    
+							    CREATE TABLE IF NOT EXISTS {$table_name_rsp} (
+								`rsp_id` int(20) NOT NULL AUTO_INCREMENT,
+								`msg_id` int(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`ip` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`content` text COLLATE utf8mb4_unicode_ci NOT NULL,		
+								`date` datetime  DEFAULT CURRENT_TIMESTAMP NOT NULL,		
+								`read_by` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`read_date` datetime  DEFAULT CURRENT_TIMESTAMP,		
+								`read_` int(10) COLLATE utf8mb4_unicode_ci NOT NULL, 
+								`reader_ip` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`rsp_by` int(1) COLLATE utf8mb4_unicode_ci NOT NULL, 
+								PRIMARY KEY  (rsp_id)
+							    ) {$charset_collate};
 
-						$sql = "CREATE TABLE IF NOT EXISTS {$table_name_status} (
-							`id` int(20) NOT NULL AUTO_INCREMENT,
-							`sid` varchar(21) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`fid` int(11)   NOT NULL, 
-							`type_` int(8)  NOT NULL,
-							`date` datetime  DEFAULT CURRENT_TIMESTAMP NOT NULL,		
-							`status` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`ip` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`os` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-							`browser` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,						
-							`read_date` datetime  DEFAULT CURRENT_TIMESTAMP,		
-							`uid` int(10)  NOT NULL, 
-							`tc` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,	
-							`active` int(1)   NOT NULL,						
-							PRIMARY KEY  (id)
-						) {$charset_collate};";
-			
-					require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-					dbDelta( $sql );
-			
+							    CREATE TABLE IF NOT EXISTS {$table_name_rsp} (
+								`id` int(20) NOT NULL AUTO_INCREMENT,
+								`sid` varchar(21) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`fid` int(11)   NOT NULL, 
+								`type_` int(8)  NOT NULL,
+								`date` datetime  DEFAULT CURRENT_TIMESTAMP NOT NULL,		
+								`status` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`ip` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`os` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+								`browser` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,						
+								`read_date` datetime  DEFAULT CURRENT_TIMESTAMP,		
+								`uid` int(10)  NOT NULL, 
+								`tc` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,	
+								`active` int(1)   NOT NULL,						
+								PRIMARY KEY  (id)
+							    ) {$charset_collate};
+	   					";
+		
+						require_once $upgrade_path;
+						dbDelta($sql);
 					
 					/* foreach($it as $path) {
 						if (preg_match("/\bbootstrap+.+.css+/i", $path)) 
