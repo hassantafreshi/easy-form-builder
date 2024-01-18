057a31eb7cca4d07ee5a0bb2c67dd05c1fb2c991
Hassan Tafreshi <hasan.tafreshi@gmail.com>
2024-01-18 06:13:29 +0330

    WP-Optimize-test

--- includes/class-Emsfb-public.php
+++ includes/class-Emsfb-public.php
@@ -5929,6 +5929,12 @@ class _Public {
 			//auto-Optimize not check
 			autoptimize_filter_js_exclude(['jquery.min.js','core.js']);
 			autoptimize_filter_js_noptimize();
+		}elseif(class_exists('WPO_Page_Cache')){
+			//w3-total-cache not check
+			error_log('delete_single_post_cache');
+			WPO_Page_Cache::delete_single_post_cache($post_id);
+		}else{
+			error_log('no cache plugin!');
 		}
 		//WP-Optimize
 	}
