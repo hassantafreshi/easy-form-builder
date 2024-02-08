e4a7a3529be9e405ccf8107b66c509d5cea38b27
Hassan Tafreshi <hasan.tafreshi@gmail.com>
2024-02-08 19:24:59 +0330

    comper_version_efb

--- includes/class-Emsfb-public.php
+++ includes/class-Emsfb-public.php
@@ -247,11 +247,12 @@ class _Public {
 					$el_pro_load = strpos($value , '"pro":"1"');
 				}
 		$el_pro_load = $el_pro_load==false ? false : true;
-		$statt= version_compare(EMSFB_PLUGIN_VERSION,$rp[1]["version"]);
+		/* $statt= version_compare(EMSFB_PLUGIN_VERSION,$rp[1]["version"]);
 		if(version_compare(EMSFB_PLUGIN_VERSION,$rp[1]["version"])!=0 ){
 			$efbFunction = new efbFunction();
 			$efbFunction->setting_version_efb_update('null' ,$this->pro_efb);
-		}
+		} */
+		$this->comper_version_efb($rp[1]["version"]);
 	
 		if(gettype($stng)=="integer" && $stng==0){
 			$stng=$lanText["settingsNfound"];
@@ -466,13 +467,17 @@ class _Public {
 		//Confirmation Code show
 		$lang = get_locale();
 		$lang =strpos($lang,'_')!=false ? explode( '_', $lang )[0]:$lang;
-				$efbFunction = empty($this->efbFunction) ? new efbFunction() :$this->efbFunction ; 
+				//$efbFunction = empty($this->efbFunction) ? new efbFunction() :$this->efbFunction ;
+				if(empty($this->efbFunction)){ 
+					$this->efbFunction = new efbFunction();
+				}
 				//translate v2
 				$text=["atcfle","cpnnc","tfnapca", "icc","cpnts","cpntl","mcplen","mmxplen","mxcplen","mmplen","offlineSend","message","clsdrspn","createdBy","easyFormBuilder","payAmount","payment","id","methodPayment","ddate","updated","methodPayment","interval","file","videoDownloadLink","downloadViedo","pWRedirect","eJQ500","error400","errorCode","remove","minSelect","search","MMessageNSendEr","formNExist","settingsNfound","formPrivateM","pleaseWaiting","youRecivedNewMessage","WeRecivedUrM","thankFillForm","trackNo","thankRegistering","welcome","thankSubscribing","thankDonePoll","error403","errorSiteKeyM","errorCaptcha","pleaseEnterVaildValue","createAcountDoneM","incorrectUP","sentBy","newPassM","done","surveyComplatedM","error405","errorSettingNFound","errorMRobot","enterVValue","guest","cCodeNFound","errorFilePer","errorSomthingWrong","nAllowedUseHtml","messageSent","offlineMSend","uploadedFile","interval","dayly","weekly","monthly","yearly","nextBillingD","onetime","proVersion","payment","emptyCartM","transctionId","successPayment","cardNumber","cardExpiry","cardCVC","payNow","payAmount","selectOption","copy","or","document","error","somethingWentWrongTryAgain","define","loading","trackingCode","enterThePhone","please","pleaseMakeSureAllFields","enterTheEmail","formNotFound","errorV01","enterValidURL","password8Chars","registered","yourInformationRegistered","preview","selectOpetionDisabled","youNotPermissionUploadFile","pleaseUploadA","fileSizeIsTooLarge","documents","image","media","zip","trackingForm","trackingCodeIsNotValid","checkedBoxIANotRobot","messages","pleaseEnterTheTracking","alert","pleaseFillInRequiredFields","enterThePhones","pleaseWatchTutorial","somethingWentWrongPleaseRefresh","formIsNotShown","errorVerifyingRecaptcha","orClickHere","enterThePassword","PleaseFillForm","selected","selectedAllOption","field","sentSuccessfully","thanksFillingOutform","sync","enterTheValueThisField","thankYou","login","logout","YouSubscribed","send","subscribe","contactUs","support","register","passwordRecovery","info","areYouSureYouWantDeleteItem","noComment","waitingLoadingRecaptcha","itAppearedStepsEmpty","youUseProElements","fieldAvailableInProversion","thisEmailNotificationReceive","activeTrackingCode","default","defaultValue","name","latitude","longitude","previous","next","invalidEmail","aPIkeyGoogleMapsError","howToAddGoogleMap","deletemarkers","updateUrbrowser","stars","nothingSelected","availableProVersion","finish","select","up","red","Red","sending","enterYourMessage","add","code","star","form","black","pleaseReporProblem","reportProblem","ddate","serverEmailAble","sMTPNotWork","aPIkeyGoogleMapsFeild","download","copyTrackingcode","copiedClipboard","browseFile","dragAndDropA","fileIsNotRight","on","off","lastName","firstName","contactusForm","registerForm","entrTrkngNo","response","reply","by","youCantUseHTMLTagOrBlank","rnfn","fil",'stf'];				
-				$text= $efbFunction->text_efb($text) ;
+				$text= $this->efbFunction->text_efb($text) ;
 		$state="tracker";
 		$pl= $this->get_setting_Emsfb('pub');
 		$stng= $pl[0];
+		
 		
 		if(gettype($stng)=="integer" && $stng==0){
 			$stng=$text["settingsNfound"];
@@ -497,13 +502,21 @@ class _Public {
 				wp_enqueue_script('googleMaps-js');
 			}
 		}
+
+		$this->pro_efb = $valstng->pro;
+		$this->comper_version_efb($pl[1]["version"]);
+		/* if(version_compare(EMSFB_PLUGIN_VERSION,$pl[1]["version"])!=0 ){
+			error_log('version_compare');
+			$this->efbFunction = new efbFunction();
+			$this->efbFunction->setting_version_efb_update('null' ,$this->pro_efb);
+		} */
 					
 		if($this->pro_efb==1){
  
 			wp_enqueue_script('efb-pro-els', EMSFB_PLUGIN_URL . 'includes/admin/assets/js/pro_els.js',false,'3.7.7');
 		 
 		}
-		//$location = $this->pro_efb==true  ? $efbFunction->get_geolocation() :'';
+		//$location = $this->pro_efb==true  ? $this->efbFunction->get_geolocation() :'';
 		$location = '';
 		//efb_code_validate_create( $fid, $type, $status, $tc)
 		$sid = $this->efbFunction->efb_code_validate_create( 0 , 0, 'visit' , 0);
@@ -5999,6 +6012,15 @@ class _Public {
 			do_action( 'wphb_clear_page_cache', $page_id );
 		}
 
+	}
+
+	public function comper_version_efb($v){
+
+		if(version_compare(EMSFB_PLUGIN_VERSION,$v)!=0 ){
+			error_log('version_compare'.$v);
+			$efbFunction = empty($this->efbFunction) ? new efbFunction() :$this->efbFunction ;
+			$efbFunction->setting_version_efb_update('null' ,$this->pro_efb);
+		}
 	}
 	
 
