212486e0e32304114c4a424387f983b4e2754373
Hassan Tafreshi <hasan.tafreshi@gmail.com>
2024-02-21 18:23:52 +0330

    v3.7.12

--- includes/admin/assets/js/list_form.js
+++ includes/admin/assets/js/list_form.js
@@ -2345,7 +2345,7 @@ function EmailTemp1Efb() {
 
 function EmailTemp2Efb() {
   return `<html xmlns='http://www.w3.org/1999/xhtml'> <body> <style> body {margin:auto 100px;${efb_var.rtl == 1 ? `direction:rtl;` : ''}}</style><center>
-<table class='efb body-wrap' style='text-align:center;width:86%;font-family:arial,sans-serif;border:12px solid rgba(126, 122, 122, 0.08);border-spacing:4px 20px;'> <tr>
+<table class='efb body-wrap' style='text-align:center;width:100%;font-family:arial,sans-serif;border:12px solid rgba(126, 122, 122, 0.08);border-spacing:4px 20px;'> <tr>
           <img src='${efb_var.images.emailTemplate1}' style='width:36%;'>
 </tr> <tr> <td><center> <table bgcolor='#FFFFFF' width='80%'' border='0'>  <tbody> <tr>
 <td style='font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:13px;color:#202020;line-height:1.5'>
