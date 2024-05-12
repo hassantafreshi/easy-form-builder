10c02d3a98e82712b31620794437956809fe287b
Hassan Tafreshi <hasan.tafreshi@gmail.com>
2024-05-12 07:46:13 +0330

    3.7.25

--- includes/admin/assets/js/new-efb.js
+++ includes/admin/assets/js/new-efb.js
@@ -3006,7 +3006,7 @@ function fun_emsFormBuilder_show_messages(content, by, userIp, track, date) {
   const ipSection = userIp!='' ? `<p class="efb small fs-7 mb-0"><span>${efb_var.text.ip}:</span> ${userIp}</p>` :''
   if (by == 1) {
      by = 'Admin'; by=`<span>${efb_var.text.by}:</span> ${by}`; } 
-  else if (by =='' ||(efb_var.hasOwnProperty('user_name') &&  efb_var.user_name.length > 1)){ 
+  else if (by ==''  && (efb_var.hasOwnProperty('user_name') &&  efb_var.user_name.length > 1)){ 
     //console.log('by admin or null',efb_var.user_name.length,efb_var.user_name.length > 1 ,efb_var.user_name );
     by = efb_var.hasOwnProperty('user_name') &&  efb_var.user_name.length > 1 ? `<span>${efb_var.text.by}:</span> ${efb_var.user_name}`  : `<span>${efb_var.text.by}:</span> ${efb_var.text.guest}`;
   }else if(by==-1){
