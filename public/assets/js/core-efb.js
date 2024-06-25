38a81b1c01eab8eb131c351ef1eb59b31e64218f
Hassan Tafreshi <hasan.tafreshi@gmail.com>
2024-06-25 17:08:20 +0330

    3.7.34

--- public/assets/js/core-efb.js
+++ public/assets/js/core-efb.js
@@ -1124,8 +1124,8 @@ document.addEventListener("DOMContentLoaded", function() {
   let elements = document.querySelectorAll('#body_efb');
   const msg = `<h3 class="efb fs-5 text-center text-dark bg-warning m-3 p-3">${ajax_object_efm.text.fetf} <div class='efb mt-1 fs-6'> ${ajax_object_efm.text.easyFormBuilder}</div> </h3>`
   if( (document.getElementById('body_efb') && document.getElementById('body_tracker_emsFormBuilder'))){
-    document.getElementById('body_efb').innerHTML =msg;
-    document.getElementById('body_efb-track').innerHTML =msg;        
+    if(document.getElementById('body_efb')) document.getElementById('body_efb').innerHTML =msg;
+    if(document.getElementById('body_efb-track')) document.getElementById('body_efb-track').innerHTML =msg;          
   }else if (elements.length > 1){
     elements[0].innerHTML =msg;
     elements[1].innerHTML =msg;
