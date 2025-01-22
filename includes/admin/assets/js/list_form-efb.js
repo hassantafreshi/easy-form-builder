54121b7b168d139d4dc67c032de39d9b92f39fd5
Hassan Tafreshi <hasan.tafreshi@gmail.com>
2025-01-22 07:50:17 +0330

    fixBugOfExportMaps&filres

--- includes/admin/assets/js/list_form-efb.js
+++ includes/admin/assets/js/list_form-efb.js
@@ -1882,8 +1882,16 @@ function fun_export_rows_for_Subscribe_emsFormBuilder(value) {
           if (content[c].type == 'payment') {
             const vx = rows[0].findIndex(x => x == "TID");
             rows[parseInt(i_count)][parseInt(vx)] = content[c].paymentIntent;
-          }else if(content[c].type == 'file' || content[c].type == 'media' || content[c].type == 'zip'  || content[c].type == 'image' || content[c].type == 'document' || content[c].type == 'allformat'){
+          }else if(content[c].value == '@file@' || content[c].type == 'file' || content[c].type == 'media' || content[c].type == 'zip'  || content[c].type == 'image' || content[c].type == 'document' || content[c].type == 'allformat'){
             rows[parseInt(i_count)][parseInt(value_col_index)] =content[c].url;
+          }else if (content[c].type == "maps"){
+            let val =''
+            content[c].value.forEach(r => {
+              const address = r.address.replaceAll(',' ,' -');
+              console.log(address)
+              val=='' ? val = `${efb_var.text.latitude}:${r.lat}; ${efb_var.text.longitude}:${r.lng}; ${efb_var.text.address}:${address}`  : val +=`| ${efb_var.text.latitude}:${r.lat}; ${efb_var.text.longitude}:${r.lng}; ${efb_var.text.address}:${address}`
+            });
+            rows[parseInt(i_count)][parseInt(value_col_index)] = val;
           }
         } else if (content[c].type == 'multiselect' || content[c].type == 'payMultiselect') {
           if (rows[i_count][0] == "null@EFB") rows[i_count][0] = v.msg_id;
