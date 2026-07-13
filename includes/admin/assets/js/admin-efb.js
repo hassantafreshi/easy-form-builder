1e91b9b6c0558643980e4625666c09fc5f2e34b8
Hassan Tafreshi <hasan.tafreshi@gmail.com>
2026-07-13 05:31:08 +0300

    updated Pro Box Noti

--- includes/admin/assets/js/admin-efb.js
+++ includes/admin/assets/js/admin-efb.js
@@ -1277,16 +1277,18 @@ function head_introduce_efb(state) {
   const link = state == "create" ? '#form' : 'admin.php?page=Emsfb_create'
   let text = `${efb_var.text.efbIsTheUserSentence} ${efb_var.text.efbYouDontNeedAnySentence}`
   let btnSize = mobile_view_efb ? '' : 'btn-lg';
-  const domain = efb_var.hasOwnProperty('wsteamDomain') ? 'https://' + efb_var.wsteamDomain +'/pricing' : 'https://whitestudio.team/#pricing';
-  let msgpro = efb_var.text.yFreeVEnPro.replace('%2$s', pro_price_efb +'$').replace('%1$s','<span class="efb fw-bold text-pinkEfb">').replace('%3$s','</span>').replace('%4$s',`<br><a href="${domain}" target="_blank" class="efb fw-bold">`).replace('%5$s','</a>');
+  const domain = efb_var.hasOwnProperty('wsteamDomain') ? 'https://' + efb_var.wsteamDomain +'/price' : 'https://whitestudio.team/#price';
+  let msgpro = efb_var.text.yFreeVEnPro.replace('%2$s', pro_price_efb +'$').replace('%1$s','<span class="efb fw-bold text-pinkEfb">').replace('%3$s','</span>').replace('%4$s',`<br><a href="${domain}" target="_blank" rel="noopener noreferrer" class="efb fw-bold efb-pro-notice__link">`).replace('%5$s','</a>');
   if(efb_var.language =='fa_IR'){
-    msgpro = efb_var.text.yFreeVEnPro.replace('%2$s',  '1,300,000<small>تومان</small>').replace('%1$s','<span class="efb fw-bold text-pinkEfb">').replace('%3$s','</span>').replace('%4$s',`<br><a href="https://easyformbuilder.ir/pricing'" target="_blank" class="efb fw-bold">`).replace('%5$s','</a>');
+    msgpro = efb_var.text.yFreeVEnPro.replace('%2$s',  '1,300,000<small>تومان</small>').replace('%1$s','<span class="efb fw-bold text-pinkEfb">').replace('%3$s','</span>').replace('%4$s',`<br><a href="https://easyformbuilder.ir/pricing" target="_blank" rel="noopener noreferrer" class="efb fw-bold efb-pro-notice__link">`).replace('%5$s','</a>');
   }
   let cont = ``;
-  let vType = `<div class="efb mx-3 col-lg-4 mt-2 pd-5 col-md-10 col-sm-12 alert alert-light pointer-efb buy-noti ec-efb" data-eventform="links" data-linkname="price">
-  <i class="efb bi-diamond text-pinkEfb mx-1"></i>
-  <span class="efb text-dark fs-7">${efb_var.text.getPro}</span><br>
-  <div class="efb ms-3 fs-7">${msgpro}</div>
+  let vType = `<div class="efb mx-3 col-lg-4 mt-2 pd-5 col-md-10 col-sm-12 alert alert-light pointer-efb buy-noti efb-pro-notice ec-efb" data-eventform="links" data-linkname="price">
+  <div class="efb d-flex align-items-center efb-pro-notice__heading">
+    <span class="efb efb-pro-notice__icon"><i class="efb bi-diamond"></i></span>
+    <span class="efb efb-pro-notice__title">${efb_var.text.getPro}</span>
+  </div>
+  <div class="efb efb-pro-notice__body">${msgpro}</div>
   </div>`;
   if(noti_exp_efb!='' && noti_exp_efb!='null'){vType='<div class="efb col-lg-4">'+noti_exp_efb+'</div>';}
   if (state != "create") {
